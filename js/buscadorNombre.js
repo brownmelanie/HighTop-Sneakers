807cf8578ec51cc94da8a19527ea2ea8357656fc
Melanie Brown <melaniebrown.bm@gmail.com>
2023-05-21 15:47:02 -0300

    modificaciones segunda preentrega

---
+++ js/buscadorNombre.js
@@ -0,0 +1,27 @@
+const productos = [
+    {nombre: "Adidas Forum 84 Low", precio: 58999},
+    {nombre: "Adidas Ultraboost Light", precio: 89999},
+    {nombre: "Adidas Rivarly Low", precio: 55999},
+    {nombre: "Nike Dunk High", precio: 59999},
+    {nombre: "Nike Air Presto", precio: 69999},
+    {nombre: "Nike Metcon 8", precio: 63999},
+    {nombre: "New Balance 550", precio: 55149},
+    {nombre: "New Balance X-Racer", precio: 48449},
+    {nombre: "New Balance 574", precio: 37349},
+  ];
+
+let buscarNombre = prompt("Ingrese el nombre del producto que desea buscar");
+  
+let resultados = [];
+productos.forEach(producto => {
+    if (producto.nombre.toLowerCase().includes(buscarNombre.toLowerCase())) {
+        resultados.push(producto);
+    }
+});
+  
+if(resultados.length === 0){
+    alert("No se encontraron productos con ese nombre.");
+}else {
+    alert("Los productos encontrados con el nombre ingresado son:\n\n" + resultados.map(producto => `${producto.nombre}, $${producto.precio}`).join("\n"));
+}
+
