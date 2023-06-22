2687252ed553aa83e2dfd48e5248fab1f8ebc6f2
Melanie Brown <melaniebrown.bm@gmail.com>
2023-06-22 03:32:41 -0300

    ajustes entrega final

--- js/iniciarSesion.js
+++ js/iniciarSesion.js
@@ -4,6 +4,18 @@ document.getElementById("registrar").addEventListener("click", () => {
   const contraseña = document.getElementById("contraseña").value;
 
   const usuariosRegistrados = JSON.parse(localStorage.getItem("usuarios")) || [];
+  
+  //Verifica si el email ya está registrado
+  const emailRegistrado = usuariosRegistrados.find(usuario => usuario.email === email);
+  if (emailRegistrado) {
+    Swal.fire({
+      icon: 'error',
+      title: 'El email ingresado ya se encuentra registrado',
+    });
+    return;
+  }
+
+  //guardo el nuevo usuario en el localStorage
   const nuevoUsuario = {
     nombreyapellido: nombre,
     email,
@@ -14,4 +26,11 @@ document.getElementById("registrar").addEventListener("click", () => {
   localStorage.setItem("usuarios", JSON.stringify(usuariosRegistrados));
   localStorage.setItem("nuevoUsuario", JSON.stringify(nuevoUsuario));
   console.log(usuariosRegistrados);
+
+  Swal.fire({
+    icon: 'success',
+    title: 'Tu usuario ha sido registrado',
+    showConfirmButton: false,
+    timer: 1500
+  });
 });
--- js/productos.js
+++ js/productos.js
@@ -1,62 +1,101 @@
-//cargo los productos
-const productos = [
-  {nombre:"Adidas Forum 84 Low", precio:58999, imagen:"../img/forumLow.jpg"},
-  {nombre:"Adidas Ultraboost Light", precio:89999, imagen:"../img/ultraboost.jpg"},
-  {nombre:"Adidas Rivarly Low", precio:55999, imagen:"../img/rivarly.jpg"},
-  {nombre:"Nike Dunk High", precio:59999, imagen:"../img/dunk.jpg"},
-  {nombre:"Nike Air Presto", precio:69999, imagen:"../img/presto.jpg"},
-  {nombre:"Nike Metcon 8", precio:63999, imagen:"../img/metcon.jpg"},
-  {nombre:"New Balance 550", precio:55149, imagen:"../img/550.jpg"},
-  {nombre:"New Balance X-Racer", precio:48449, imagen:"../img/racer.jpg"},
-  {nombre:"New Balance 574", precio:37349, imagen: "../img/574.jpg"},
-];
+//cargo los productos en .json y los muestro en un div
+const traerDatos = async () => {
+  const response = await fetch("../data.json");
+  const data = await response.json();
+  let productos = data; //mantiene una copia original de todos los productos para que el carrito tambien funcione despues de usar el buscador
 
-//muestro los productos en el HTML
-const sectionContainer = document.querySelector("section.shop");
-productos.forEach((producto) => {
-  let div = document.createElement("div");
-  div.innerHTML = `
-  <div class="card">
-      <img src="${producto.imagen}" alt="${producto.nombre}">
-      <h3 class="titulo">${producto.nombre}</h3>
-      <div class="precioContainer">
-          <b>$</b>
-          <b class="precio">${producto.precio}</b>
-      </div>
-      <button id="boton1">Comprar</button>
-  </div>
-  `
-  sectionContainer.appendChild(div);
-});
+  function mostrarProductos(productos) {
+    shop.innerHTML = ""; //limpia los productos actuales
 
-//carrito
-const comprar = document.querySelectorAll("button");
-let carrito = [];
+    if (productos.length === 0) {
+      //muestra un mensaje si no se encontraron resultados
+      const mensaje = document.createElement("p");
+      mensaje.textContent = "No se encontraron productos.";
+      shop.appendChild(mensaje);
+    } else {
+      //muestra los productos encontrados
+      productos.forEach((publicacion) => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+          <div class="card">
+            <img src="${publicacion.img}" alt="${publicacion.nombre}">
+            <h2 class="titulo">${publicacion.nombre}</h2>
+            <div class="precioContainer">
+              <b>$</b>
+              <b class="precio">${publicacion.precio}</b>
+            </div>
+            <button class="comprar">Comprar</button>
+          </div>
+        `;
+        shop.append(div);
+      });
+    };
 
-comprar.forEach(button => {
-  button.addEventListener("click", agregarCarrito);
-});
-
-function agregarCarrito(e) {
-  const button = e.target;
-  const item = button.closest('.card');
-  const titulo = item.querySelector('.titulo').textContent;
-  const precio = parseFloat(item.querySelector('.precio').textContent);
-  const nuevoItem = {
-    titulo: titulo,
-    precio: precio,
-    cantidad: 1,
+    //vincula la funcionalidad de agregar al carrito a los nuevos button de comprar (a los que se generan cuando se usa el buscador)
+    const comprar = document.querySelectorAll(".comprar");
+    comprar.forEach((button) => {
+      button.addEventListener("click", agregarCarrito);
+    });
   };
-  agregarItem(nuevoItem);
+
+  //funcion que filtra los productos que coinciden con lo que se busco
+  function buscarProductos() {
+    const searchTerm = searchInput.value.trim().toLowerCase();
+    const resultados = data.filter((publicacion) =>
+      publicacion.nombre.toLowerCase().includes(searchTerm)
+    );
+    mostrarProductos(resultados);
+  };
+
+  const shop = document.getElementById("shop");
+  const searchInput = document.querySelector(".search");
+  const btnBuscar = document.getElementById("btnBuscar");
+  mostrarProductos(productos);
+
+  btnBuscar.addEventListener("click", (event) => {
+    event.preventDefault(); //evita la recarga de la página, sin esto buscaba los productos pero se borraban a los milisegundos
+    buscarProductos();
+  });
+  searchInput.addEventListener("input", buscarProductos);
+
+
+  //creo el carrito como un array vacío para agregar los productos seleccionados
+  let carrito = [];
+  let total = 0;
+
+  function agregarCarrito(e) {
+    const button = e.target;
+    const item = button.closest(".card");
+    const titulo = item.querySelector(".titulo").textContent;
+    const precio = parseFloat(item.querySelector(".precio").textContent);
+    const nuevoItem = {
+      titulo: titulo,
+      precio: precio,
+    };
+
+    
+    //buscar el título del producto en el archivo JSON para poder mostrarlo en el alerta de SweetAlert, sino sale como object
+    const productoEncontrado = data.find(
+      (publicacion) => publicacion.nombre === titulo
+    );
+    const tituloProducto = productoEncontrado
+      ? productoEncontrado.nombre
+      : "Producto Desconocido";
+
+    carrito.push(nuevoItem);
+    total += precio;
+    console.log("Se agregó al carrito:", nuevoItem);
+    console.log("Carrito actual:", carrito, "Total: $" + total);
+
+    Swal.fire({
+      icon: "success",
+      title: "Agregado al carrito",
+      text: `Producto: ${tituloProducto}`,
+      footer: `Total del carrito: $${total}`,
+    });
+  };
 };
 
-function agregarItem(nuevoItem) {
-  carrito.push(nuevoItem);
-  renderCarrito();
-};
+traerDatos();
 
-function renderCarrito() {
-  console.log(carrito);
-  let total = carrito.reduce((acum, item) => acum + item.precio, 0);
-  console.log("Total: $" + total); //MUESTRA EL TOTAL DEL CARRITO EN CONSOLA, no logre ponerlo como document.write para que lo muestre en pantalla
-};
+
